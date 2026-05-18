f81ac383f9dd7ee57a00133d9285065a10f64560
David Schilling <dschilling@churchtools.de>
2026-05-18 08:58:18 +0200

    Avoid early MatrixClientPeg access in ready stores

--- apps/web/src/stores/ReadyWatchingStore.ts
+++ apps/web/src/stores/ReadyWatchingStore.ts
@@ -9,7 +9,6 @@
 import { type MatrixClient, SyncState } from "matrix-js-sdk/src/matrix";
 import { EventEmitter } from "events";
 
-import { MatrixClientPeg } from "../MatrixClientPeg";
 import { type ActionPayload } from "../dispatcher/payloads";
 import { type IDestroyable } from "../utils/IDestroyable";
 import { Action } from "../dispatcher/actions";
@@ -26,8 +25,9 @@ export abstract class ReadyWatchingStore extends EventEmitter implements IDestro
     public async start(): Promise<void> {
         this.dispatcherRef = this.dispatcher.register(this.onAction);
 
-        // MatrixClientPeg can be undefined in tests because of circular dependencies with other stores
-        const matrixClient = MatrixClientPeg?.get();
+        // MatrixClientPeg can be undefined in tests and during early store initialisation because of circular
+        // dependencies with other stores.
+        const matrixClient = window.mxMatrixClientPeg?.get();
         if (matrixClient) {
             this.matrixClient = matrixClient;
             await this.onReady();
